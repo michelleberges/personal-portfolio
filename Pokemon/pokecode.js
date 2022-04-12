1a6422d6d3118736fa97246d76bc4b1fb57975ec
michelleberges <berges.k.michelle@gmail.com>
2022-04-12 14:41:33 -0600

    added card flip feature and some styling

--- Pokemon/pokecode.js
+++ Pokemon/pokecode.js
@@ -7,17 +7,27 @@ const getAPIData = async (url) => {
     }
   }
   
-  async function loadPokemon() {
-    const pokeData = await getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`)
-  populatePokeGrid(pokeData)
+  async function loadPokemon(offset = 0, limit = 25) {
+    const pokeData = await getAPIData(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+    for (const nameAndUrl of pokeData.results) {
+      const pokemon = await getAPIData(nameAndUrl.url)
+      populatePokeCard(pokemon)
+    }
   }
   
   const pokeGrid = document.querySelector('.pokeGrid')
   
   function populatePokeGrid(pokemonArray) {
-    // loop through all of the pokemon and create individual pokeCards
-    populatePokeCard(pokemonArray[0])
+    const pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    const pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
     
+    pokeCard.appendChild(populateCardFront(pokemon))
+    pokeCard.appendChild(populateCardBack(pokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
   }
   
   function populatePokeCard(pokemon) {
@@ -34,11 +44,11 @@ const getAPIData = async (url) => {
   
   function populateCardFront(pokemon) {
     const pokeFront = document.createElement('figure')
-    pokeFront.className = 'cardFace'
+    pokeFront.className = 'cardFace front'
     const pokeImg = document.createElement('img')
-    pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png`
+    pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
     const pokeCaption = document.createElement('figcaption')
-    pokeCaption.textContent = 'snorlax'
+    pokeCaption.textContent = pokemon.name
   
     pokeFront.appendChild(pokeImg)
     pokeFront.appendChild(pokeCaption)
@@ -46,7 +56,12 @@ const getAPIData = async (url) => {
   }
   
   function populateCardBack(pokemon) {
-  
+    const pokeBack = document.createElement('div')
+    pokeBack.className = 'cardFace back'
+    const label = document.createElement('h4')
+    label.textContent = 'Abilities'
+    pokeBack.appendChild(label)
+    return pokeBack
   }
   
-  loadPokemon()
+  loadPokemon(700, 50)
