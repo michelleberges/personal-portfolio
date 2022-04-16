0fec6ffa4e1e6303f035dcefda04e4bb6e2046ee
michelleberges <berges.k.michelle@gmail.com>
2022-04-15 22:07:13 -0600

    Added new pokemon contructor & pokeball image

--- Pokemon/pokecode.js
+++ Pokemon/pokecode.js
@@ -14,21 +14,57 @@ const getAPIData = async (url) => {
       populatePokeCard(pokemon)
     }
   }
+  class Pokemon {
+    constructor(name, height, weight, abilities, types) {
+        this.id = 9001,
+        this.name = name,
+        this.height = height,
+        this.weight = weight,
+        this.abilities = abilities,
+        this.types = types
+    }
+  }
+
+  const newButton = document.createElement('button')
+  newButton.textContent = 'New Pokemon'
+  const header = document.querySelector('header')
+  header.appendChild(newButton)
+  newButton.addEventListener('click', () => {
+  
+    const pokeName = prompt('What is the name of your new Pokemon?', 'Thoremon')
+    const pokeHeight = prompt("What is the Pokemon's height?", 20)
+    const pokeWeight = prompt("What is the Pokemon's weight?", 1000)
+    const pokeAbilities= prompt("What are your Pokemon's abilities? (use a comman-separated list)")
+    const pokeTypes = prompt("What are your Pokemon's types? (up to 2 types separated by a space)")
+  
+    const newPokemon = new Pokemon(
+      pokeName,
+      pokeHeight,
+      pokeWeight,
+      makeAbilitiesArray(pokeAbilities),
+      makeTypesArray(pokeTypes),
+    )
+    console.log(newPokemon)
+    populatePokeCard(newPokemon)
+  })
+  function makeAbilitiesArray(commaString) {
+    return commaString.split(',').map((abilityName) => {
+      return {
+        ability: { name: abilityName}
+      }
+    })
+  }
+  
+  function makeTypesArray(spacedString) {
+    return spacedString.split(' ').map((typeName) => {
+      return {
+        type: { name: typeName}
+      }
+    })
+  }
   
   const pokeGrid = document.querySelector('.pokeGrid')
   
-  function populatePokeGrid(pokemonArray) {
-    const pokeScene = document.createElement('div')
-    pokeScene.className = 'scene'
-    const pokeCard = document.createElement('div')
-    pokeCard.className = 'card'
-    pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
-    
-    pokeCard.appendChild(populateCardFront(pokemon))
-    pokeCard.appendChild(populateCardBack(pokemon))
-    pokeScene.appendChild(pokeCard)
-    pokeGrid.appendChild(pokeScene)
-  }
   
   function populatePokeCard(pokemon) {
     const pokeScene = document.createElement('div')
@@ -38,6 +74,7 @@ const getAPIData = async (url) => {
     pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
   
     pokeCard.appendChild(populateCardFront(pokemon))
+    pokeCard.appendChild(populateCardBack(pokemon))
     pokeScene.appendChild(pokeCard)
     pokeGrid.appendChild(pokeScene)
   }
@@ -46,7 +83,12 @@ const getAPIData = async (url) => {
     const pokeFront = document.createElement('figure')
     pokeFront.className = 'cardFace front'
     const pokeImg = document.createElement('img')
+    if (pokemon.id > 9000) {
+      // load local image
+      pokeImg.src = '../images/pokeball.png'
+    } else {   
     pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+    }
     const pokeCaption = document.createElement('figcaption')
     pokeCaption.textContent = pokemon.name
   
@@ -61,7 +103,14 @@ const getAPIData = async (url) => {
     const label = document.createElement('h4')
     label.textContent = 'Abilities'
     pokeBack.appendChild(label)
+    const abilityList = document.createElement('ul')
+    pokemon.abilities.forEach((abilityItem) => {
+      const listItem = document.createElement('li')
+      listItem.textContent = abilityItem.ability.name
+      abilityList.appendChild(listItem)
+    })
+    pokeBack.appendChild(abilityList)
     return pokeBack
   }
-  
+
   loadPokemon(700, 50)
